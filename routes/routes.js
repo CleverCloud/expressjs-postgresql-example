1a26ba8fc06129c8a8c0c0cc6c80535130c4c074
Clément Nivolle <clement@nivolle.com>
2022-01-17 11:10:53 +0100

    added a prime function + css updates

--- routes/routes.js
+++ routes/routes.js
@@ -8,6 +8,20 @@ router.get('/', function(req, res) {
   pg.getVal(res);
 });
 
+router.get('/prime', function(req, res) {
+  function mySlowFunction(baseNumber) {
+    console.time('mySlowFunction');
+    let result = 0;	
+    for (var i = Math.pow(baseNumber, 7); i >= 0; i--) {
+      result += Math.atan(i) * Math.tan(i);
+    };
+    console.timeEnd('mySlowFunction');
+  }
+
+  var numero = mySlowFunction(12); // higher number => more iterations => slower
+  res.send(JSON.stringify({status: "ok", value:numero}));
+});
+
 router.post('/values', function(req, res) {
   res.setHeader('Content-Type', 'application/json');
   var val = req.body.value;
